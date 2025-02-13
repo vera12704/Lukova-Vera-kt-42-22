872c2c284b26b8a0bd13e38d672a8befecaf7cc2
vera12704 <lverunchik27@gmail.com>
2025-02-13 11:43:41 +0300

    add logging and WeatherForecast test request

--- Lukova-Vera-kt-42-22/obj/Debug/net8.0/Lukova-Vera-kt-42-22.MvcApplicationPartsAssemblyInfo.cs
+++ Lukova-Vera-kt-42-22/obj/Debug/net8.0/Lukova-Vera-kt-42-22.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Lukova-Vera-kt-42-22")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d46aeb8a56ea560de56d7651545cea36f62a1cb4")]
-[assembly: System.Reflection.AssemblyProductAttribute("Lukova-Vera-kt-42-22")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Lukova-Vera-kt-42-22")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
