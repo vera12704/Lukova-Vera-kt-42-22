d23150c1f2a7425d615484c45824c5afdff9d64a
vera12704 <lverunchik27@gmail.com>
2025-10-17 18:18:41 +0300

    add logging and WeatherForecast test request

--- lukovaverakt-42-22/Controllers/WeatherForecastController.cs
+++ lukovaverakt-42-22/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using lukovaverakt_42_22;
 using Microsoft.AspNetCore.Mvc;
 
 namespace lukovaverakt_42_22.Controllers
@@ -21,6 +22,7 @@ namespace lukovaverakt_42_22.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogError("Method was called");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -28,6 +30,14 @@ namespace lukovaverakt_42_22.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+        [HttpPost(Name = "AddNewSummary")]
+        public string[] AddNewSummary(string newSummary)
+        {
+            _logger.LogError("New method was called");
+            var list = Summaries.ToList();
+            list.Add(newSummary);
+            return list.ToArray();
         }
     }
 }
